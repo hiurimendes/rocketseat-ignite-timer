972c20d771a213347d707a25c1cdc6c5f4d5089e
Hiuri Mendes <hiurimendes@gmail.com>
2024-02-21 09:41:06 -0300

    Deploy Ignite Timer

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import styles, { ButtonContainer } from './Button.styles.ts'
+
+interface ButtonProps {
+  variant?: ButtonVariant
+}
+
+export function Button({ variant = 'primary' }: ButtonProps) {
+  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
+}
